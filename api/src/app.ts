ecf9804f9c854069eaa3b7a20a4a433a6a537fdc
David <david.hcandido1@gmail.com>
2025-09-30 14:06:35 -0300

    fix(api)

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,18 +1,19 @@
-import express from 'express'
+import express from 'express';
 import cors from 'cors';
 import countriesRoutes from './routes/countriesRoutes';
 
 const app = express();
-app.use('/', countriesRoutes)
+
 const port = 3333;
 app.use(
   cors({
     origin: 'http://localhost:4200',
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
   })
 );
+app.use('/', countriesRoutes);
 app.use(express.json());
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`)
-})
+  console.log(`Server is running on http://localhost:${port}`);
+});
